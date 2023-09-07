1e2468b03be734ee7f17bcbf6d4b80621efa9bdc
Dina <lutdina04@yandex.ru>
2023-09-08 01:46:20 +1000

    18

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,66 +1,57 @@
+import React, {useState, useEffect} from "react";
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
+import img_1 from "../../Components/Img/car_1.png";
+import img_2 from "../../Components/Img/car_2.png";
+import img_3 from "../../Components/Img/car_3.png";
+import img_4 from "../../Components/Img/car_4.png";
+import img_5 from "../../Components/Img/car_5.png";
+import img_6 from "../../Components/Img/car_6.png";
+import img_7 from "../../Components/Img/car_7.png";
+import img_8 from "../../Components/Img/car_8.png";
 import "./Carousel.css";
-import { Children, cloneElement, useEffect, useState } from "react";
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
-export const Carousel = ({ children }) => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const [pages, setPages] = useState([]);
-    const [offset, setOffset] = useState(0);
+const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-    const handleWindowResize = () => {
-        setWindowWidth(window.innerWidth);
-    };
+    const handleLeftArrowClick = () => setCurrentSlide((prevSlide) => (prevSlide === 0 ? 7 : prevSlide - 1));
+    const handleRightArrowClick = () => setCurrentSlide((prevSlide) => (prevSlide === 7 ? 0 : prevSlide + 1));
 
-    const handleLeftArrowClick = () => {
-        setOffset((currentOffset) => {
-            return Math.min(currentOffset + windowWidth + (windowWidth * 3,4 / windowWidth) );
-        });
-    };
-
-    const handleRightArrowClick = () => {
-        const maxOffset = -(windowWidth * (pages.length - 1));
-        setOffset((currentOffset) => {
-            return Math.max(currentOffset - windowWidth + (windowWidth * 3,4) / windowWidth , maxOffset);
-        });
-    };
+    const images = [
+        img_1,
+        img_2,
+        img_3,
+        img_4,
+        img_5,
+        img_6,
+        img_7,
+        img_8
+    ];
 
     useEffect(() => {
-        setPages(
-            Children.map(children, (child) => {
-                return cloneElement(child, {
-                    style: {
-                        maxWidth: '100%',
-                        minWidth: '100%',
-                    },
-                })
-            })
-        )
-    }, [children])
+        const interval = setInterval(() => {
+            setCurrentSlide((prevSlide) => (prevSlide === 7 ? 0 : prevSlide + 1));
+        }, 5000);
 
-    useEffect(() => {
-        window.addEventListener("resize", handleWindowResize);
-        return () => {
-            window.removeEventListener("resize", handleWindowResize);
-        };
+        return () => clearInterval(interval);
     }, []);
 
-    useEffect(() => {
-        setOffset(0);
-    }, [windowWidth]);
-
     return (
-        <div className="main-container">
-            <FaChevronLeft className="arrow" onClick={handleLeftArrowClick} />
-            <div className="window">
-                <div className="all-items-container"
-                     style={{
-                         transform: `translateX( ${offset}px)`
-                     }}
-                >
-                    {pages}
-                </div>
+        <div className="slider">
+
+            <div className="arrow arrow-left" onClick={handleLeftArrowClick}>
+                <FaChevronLeft/>
             </div>
-            <FaChevronRight className="arrow" onClick={handleRightArrowClick} />
+
+            <div className="image-container">
+                <img src={images[currentSlide]} alt={`Image ${currentSlide + 1}`}/>
+            </div>
+
+            <div className="arrow arrow-right" onClick={handleRightArrowClick}>
+                <FaChevronRight/>
+            </div>
+
         </div>
     );
-}
+};
+
+export default Slider;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ButtonAppBar from "../Components/Header/Header";
-import {Carousel} from "../Components/Carousel/Carousel";
 import Info from "../Components/Info/Info";
 import Basement from "../Components/Basement/Basement";
 import '../App.css';
 import Catalog_list from "../Components/Catalog_list/Catalog_list";
 import Gap from "../Components/Gap/Gap"
+import Slider from "../Components/Carousel/Carousel";
 
 function Main() {
     return (
         <div className="root">
             <ButtonAppBar/>
-            <div className="content">
-                <Carousel>
-                    <div className="item-1"></div>
-                    <div className="item-2"></div>
-                    <div className="item-3"></div>
-                    <div className="item-4"></div>
-                    <div className="item-5"></div>
-                    <div className="item-6"></div>
-                    <div className="item-7"></div>
-                    <div className="item-8"></div>
-                </Carousel>
-            </div>
+            <Slider/>
+
 
             <Info/>
             <Catalog_list/>
